a2a817bcafc3c39315123575a935f4eaddc78132
Anton Stoychev <antitoxic@gmail.com>
2015-07-10 19:48:34 +0300

    More template tags

--- src/contact/templatetags/contactpoint.py
+++ src/contact/templatetags/contactpoint.py
@@ -1,7 +1,13 @@
 from django import template
+from ..models import Organisation
+from location.models import Area
+from ..apps import setting
 
 register = template.Library()
 
-@register.inclusion_tag('_stats.html', takes_context=True)
-def contactpoint_stats(context):
-    pass
+@register.inclusion_tag('_stats.html')
+def contactpoint_stats():
+    return {
+        "organisations_count": Organisation.objects.all().count(),
+        "areas_count": Area.objects.exclude(size__in=[setting('contact_address_areasize')]).count(),
+    }
