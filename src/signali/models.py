fe9dc4e0222cd922128cd26e098b98c31f32c15d
Anton Stoychev <antitoxic@gmail.com>
2015-07-25 18:47:37 +0300

    Initial work for making feature homepage areas dynamic

--- src/signali/models.py
+++ src/signali/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from accessibility.models import AbstractVisibility
 from taxonomy.models import Category, CategoryManager
+from contact.models import ContactPoint, ContactPointManager
 
 
 class Setting(models.Model):
@@ -39,9 +40,31 @@ class CategoryManagerProxy(CategoryManager):
                    .extra(select={'is_featured': Visibility._meta.db_table + '.is_featured OR NULL'})\
                    .order_by('visibility__popularity', 'is_featured')
 
+    def featured(self):
+        return self.filter(visibility__is_featured=True)
+
+
 
 class CategoryProxy(Category):
     objects = CategoryManagerProxy()
+
+    class Meta:
+        proxy = True
+
+
+class ContactPointManagerProxy(ContactPointManager):
+    def popular(self):
+        return self\
+                   .all()\
+                   .extra(select={'is_featured': Visibility._meta.db_table + '.is_featured OR NULL'})\
+                   .order_by('visibility__popularity', 'is_featured')
+
+    def featured(self):
+        return self.filter(visibility__is_featured=True)
+
+
+class ContactPointProxy(ContactPoint):
+    objects = ContactPointManagerProxy()
 
     class Meta:
         proxy = True
