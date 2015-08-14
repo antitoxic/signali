b136ac34936b74242fbd534ddcfb2532a57b6bcf
Anton Stoychev <antitoxic@gmail.com>
2015-08-14 22:35:55 +0300

    Autoload settings that reference classes by string path

--- src/signali/utils.py
+++ src/signali/utils.py
@@ -1,10 +1,17 @@
 from django.conf import settings
 from .models import Setting
+import importlib
 
-
-def setting(name):
+def setting(name, default=None):
     try:
-        return getattr(settings, name)
+        value = getattr(settings, name, default)
     except:
         site_settings = Setting.objects.all()[:1].get()
-        return getattr(site_settings, name, None)
+        value = getattr(site_settings, name, default)
+
+    if name in settings.CLASS_SETTINGS:
+        module, value = value.rsplit('.', 1)
+        module = importlib.import_module(module)
+        value = getattr(module, value, default)
+
+    return value
