12e0e486665d402555c663c929401f827677090c
Anton Stoychev <antitoxic@gmail.com>
2015-08-16 22:25:24 +0300

    User admin now extends default user admin

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     pass
 
 admin.site.register(User, UserAdmin)
