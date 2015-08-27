d9a7adc1e1b41500fcc5e5c778b78b78fce7daa1
Anton Stoychev <antitoxic@gmail.com>
2015-08-27 16:27:49 +0300

    Missing addition to the app-settings getter

--- src/signali/utils.py
+++ src/signali/utils.py
@@ -1,5 +1,6 @@
 import importlib
 
+from django.apps import apps as django_apps
 from django.conf import settings
 from .models import Setting
 
@@ -10,6 +11,9 @@ def setting(name, default=None, noparse=False):
     except:
         site_settings = Setting.objects.all()[:1].get()
         value = getattr(site_settings, name, default)
+
+    if name in settings.MODEL_SETTINGS and not noparse:
+        value = django_apps.get_model(value)
 
     if name in settings.CLASS_SETTINGS and not noparse:
         module, value = value.rsplit('.', 1)
