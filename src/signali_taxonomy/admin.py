2948cc1452372ae777ce95d6fac20e5756b4982b
Anton Stoychev <antitoxic@gmail.com>
2015-09-10 10:01:47 +0300

    Better admin for taxonomy

--- src/signali_taxonomy/admin.py
+++ src/signali_taxonomy/admin.py
@@ -18,6 +18,8 @@ class CategoryAdmin(admin.ModelAdmin):
         ('basic', _('basic')),
         ('visibility', _('visibility')),
     )
+    list_display = ('title',  'is_featured', 'is_public',)
+    list_editable = ('is_featured', 'is_public', )
     fieldsets = (
         (None, {
             'classes': ('suit-tab suit-tab-basic',),
